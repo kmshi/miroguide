77a853913b50cde5955367b70a4b84d4bf23f2fa
ben <ben@be7adf91-e322-0410-8f47-e6edb61c52aa>
2007-03-28 21:20:59 +0000

    new and improved update script.
    git-svn-id: https://svn.participatoryculture.org/svn/channelguide2/trunk@363 be7adf91-e322-0410-8f47-e6edb61c52aa

--- update.sh
+++ update.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
+echo "stopping apache"
+/etc/init.d/httpd stop
 echo "updating from SVN"
 svn up
 echo "updating database"
 python channelguide/manage.py syncdb
 echo "optimizing templates"
 python channelguide/manage.py optimize_templates
-echo "restarting apache"
-/etc/init.d/httpd restart
 echo "clearing the cache"
 python channelguide/manage.py clear_cache
+echo "starting apache"
+/etc/init.d/httpd restart
 echo "all systems go"
