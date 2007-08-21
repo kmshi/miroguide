4e5ee23f278f5607170b7fe282ed5646224ac539
pswartz <pswartz@be7adf91-e322-0410-8f47-e6edb61c52aa>
2007-08-21 18:14:30 +0000

    add db update for moderator sharing
    git-svn-id: https://svn.participatoryculture.org/svn/channelguide2/trunk@648 be7adf91-e322-0410-8f47-e6edb61c52aa

---
+++ channelguide/db/updates/26_moderator_shared_by_id.sql
@@ -0,0 +1,2 @@
+ALTER TABLE cg_channel ADD COLUMN moderator_shared_by_id INT(11) NULL;
+ALTER TABLE cg_channel ADD CONSTRAINT fk_moderator_shared_by_id FOREIGN KEY (moderator_shared_by_id) REFERENCES user (id) ON DELETE SET NULL;
