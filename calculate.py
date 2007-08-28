06b2c655e008289d3f1a1fb674c7be9147e7f852
pswartz <pswartz@be7adf91-e322-0410-8f47-e6edb61c52aa>
2007-08-28 20:45:20 +0000

    add support for calculating last two days of recommendations
    git-svn-id: https://svn.participatoryculture.org/svn/channelguide2/trunk@695 be7adf91-e322-0410-8f47-e6edb61c52aa

--- calculate.py
+++ calculate.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2.5
 from channelguide import manage, init # set up environment
-import math
+import math, sys
 init.init_external_libraries()
 
 from channelguide import db
@@ -18,15 +18,30 @@ SELECT ip_address FROM cg_channel_subscription WHERE ip_address!=%s AND
     results = database.execute(sql, (channel, "0.0.0.0", channel, 16070400))
     return [e[0] for e in results]
 
+def getRecent(seconds):
+    sql = """SELECT DISTINCT channel_id FROM cg_channel_subscription WHERE (NOW()-timestamp) < %s AND ip_address!=%s"""
+    results = database.execute(sql, (seconds, "0.0.0.0"))
+    return [e[0] for e in results]
+
 def getSimilarity(channel1, channel2):
     c = Channel()
     c.id = channel1
     return c.get_similarity(database, channel2)
 
-def main():
-    hit = set()
+def calculateAll():
     database.execute("DELETE FROM cg_channel_recommendations");
     channels = [e[0] for e in channel.select('id').execute(database)]
+    calculateRecommendations(channels)
+
+def calculateTwoDays():
+    channels = map(int, getRecent(60*60*24*2))
+    print channels
+    container = ','.join([str(x) for x in channels])
+    database.execute("DELETE FROM cg_channel_recommendations WHERE channel1_id IN (%s) OR channel2_id IN (%s)" % (container, container))
+    calculateRecommendations(channels)
+
+def calculateRecommendations(channels):
+    hit = set()
     for c1 in channels:
         gas = getAllSimilar(c1)
         if gas:
@@ -40,5 +55,9 @@ def main():
                     if gs:
                         database.execute("INSERT LOW_PRIORITY INTO cg_channel_recommendations VALUES (%s, %s, %s)", (c1, c2, gs))
             database.commit()
+
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) > 1 and sys.argv[1] == 'all':
+        calculateAll()
+    else:
+        calculateTwoDays()
