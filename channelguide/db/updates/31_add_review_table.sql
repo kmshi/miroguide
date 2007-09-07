2a804a7052eba1f75d9e5153aaa5450a32c3a3cd
pswartz <pswartz@be7adf91-e322-0410-8f47-e6edb61c52aa>
2007-09-07 16:28:17 +0000

    remove review table
    git-svn-id: https://svn.participatoryculture.org/svn/channelguide2/trunk@718 be7adf91-e322-0410-8f47-e6edb61c52aa

--- channelguide/db/updates/31_add_review_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE cg_channel_review (
-    user_id INT(11) NOT NULL,
-    channel_id INT(11) NOT NULL,
-    review TEXT UNICODE,
-    timestamp DATETIME NOT NULL,
-    PRIMARY KEY (user_id, channel_id),
-    CONSTRAINT fk_channel_review_user FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE,
-    CONSTRAINT fk_channel_review_channel FOREIGN KEY (channel_id) REFERENCES cg_channel (id) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
