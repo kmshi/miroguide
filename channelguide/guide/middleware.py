02603dbd42938bdde5cd8050d20fae4c989e1593
pswartz <pswartz@be7adf91-e322-0410-8f47-e6edb61c52aa>
2007-09-07 22:44:22 +0000

    add profiling middleware
    git-svn-id: https://svn.participatoryculture.org/svn/channelguide2/trunk@733 be7adf91-e322-0410-8f47-e6edb61c52aa

--- channelguide/guide/middleware.py
+++ channelguide/guide/middleware.py
@@ -2,12 +2,12 @@ import locale
 import logging
 import traceback
 
-from channelguide import util
+from channelguide import util, settings
 from exceptions import AuthError
 from models import Channel, User
 from models.user import AnonymousUser
 from auth import SESSION_KEY
-
+import hotshot, hotshot.stats, tempfile, StringIO, sys
 class UserMiddleware(object):
     """Add a User object to each request.
 
@@ -44,4 +44,84 @@ class ChannelCountMiddleware(object):
         channel_count = Channel.query_approved().count(request.connection)
         request.connection.commit()
         request.total_channels = locale.format('%d', channel_count, True)
+
+class ProfileMiddleware(object):
+
+    """
+
+    Displays hotshot profiling for any view.
+
+    http://yoursite.com/yourview/?prof
+
+
+
+    Add the "prof" key to query string by appending ?prof (or &prof=)
+
+    and you'll see the profiling results in your browser.
+
+    It's set up to only be available in django's debug mode,
+
+    but you really shouldn't add this middleware to any production configuration.
+
+    * Only tested on Linux
+
+    """
+
+    def process_request(self, request):
+
+        if request.has_key('prof'):
+
+            self.tmpfile = tempfile.NamedTemporaryFile()
+
+            self.prof = hotshot.Profile(self.tmpfile.name)
+
+
+
+    def process_view(self, request, callback, callback_args, callback_kwargs):
+
+        if request.has_key('prof'):
+
+            return self.prof.runcall(callback, request, *callback_args, **callback_kwargs)
+
+
+
+    def process_response(self, request, response):
+
+        if request.has_key('prof'):
+
+            self.prof.close()
+
+
+
+            out = StringIO.StringIO()
+
+            old_stdout = sys.stdout
+
+            sys.stdout = out
+
+
+
+            stats = hotshot.stats.load(self.tmpfile.name)
+
+            #stats.strip_dirs()
+
+            stats.sort_stats('cumulative', 'calls')
+
+            stats.print_stats('channelguide/')
+
+
+
+            sys.stdout = old_stdout
+
+            stats_str = out.getvalue()
+
+
+
+            if response and response.content and stats_str:
+
+                response.content = "<pre>" + stats_str + "</pre>"
+
+
+
+        return response
 
