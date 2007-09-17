bc58ff32d14604257b176a98245657c52314f37b
pswartz <pswartz@be7adf91-e322-0410-8f47-e6edb61c52aa>
2007-09-17 15:18:54 +0000

    new popular code
    git-svn-id: https://svn.participatoryculture.org/svn/channelguide2/trunk@779 be7adf91-e322-0410-8f47-e6edb61c52aa

--- channelguide/guide/views/frontpage.py
+++ channelguide/guide/views/frontpage.py
@@ -7,6 +7,13 @@ from channelguide.guide import tables
 from channelguide.guide.models import Channel, Category, PCFBlogPost
 
 def get_popular_channels(connection, count):
+    sql = "SELECT * FROM cg_channel_generated_top7'
+    try:
+        vals = connect.execute(sql)
+    except Exception:
+        vals = []
+    if vals:
+        return vals
     query = Channel.query_approved()
     query.load('subscription_count_today')
     query.order_by('subscription_count_today', desc=True).limit(count)
