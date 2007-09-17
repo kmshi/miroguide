80a593149edf129ddd16ffe9438c9b370a4cfca1
pswartz <pswartz@be7adf91-e322-0410-8f47-e6edb61c52aa>
2007-09-17 14:41:22 +0000

    updates to donate bar
    git-svn-id: https://svn.participatoryculture.org/svn/channelguide2/trunk@777 be7adf91-e322-0410-8f47-e6edb61c52aa

--- channelguide/lib/sqlhelper/connection.py
+++ channelguide/lib/sqlhelper/connection.py
@@ -10,19 +10,10 @@ class Connection(object):
     def __init__(self, raw_connection):
         self.raw_connection = raw_connection
         self.cursor = raw_connection.cursor()
-        self.logfile = file('/tmp/sql.log', 'w')
 
     def execute(self, sql, args=None):
-        self.logfile.write('executing %r\n' % sql)
-        if args:
-            self.logfile.write('with args: %r\n' % (args,))
-        s = time.time()
         self.cursor.execute(sql, args)
-        o = time.time()
-        self.logfile.write('execute took %f seconds\n' % (o-s))
-        rows = self.cursor.fetchall()
-        self.logfile.write('fetchall took %f seconds\n' % (time.time()-o))
-        return rows
+        return self.cursor.fetchall()
 
     def commit(self):
         self.raw_connection.commit()
@@ -33,7 +24,6 @@ class Connection(object):
     def close(self):
         self.cursor.close()
         self.raw_connection.close()
-        self.logfile.close()
 
     def get_lastrowid(self):
         return self.cursor.lastrowid
