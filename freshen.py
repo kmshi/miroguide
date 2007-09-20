880d3029c2ef2e97c19232d7e31c3eb27e2363d1
pswartz <pswartz@be7adf91-e322-0410-8f47-e6edb61c52aa>
2007-09-20 20:08:02 +0000

    freshen script
    git-svn-id: https://svn.participatoryculture.org/svn/channelguide2/trunk@829 be7adf91-e322-0410-8f47-e6edb61c52aa

--- freshen.py
+++ freshen.py
@@ -1,6 +1,6 @@
 from channelguide import manage, init
 init.init_external_libraries()
-from channelguide.guide.views import channels
+from channelguide.guide.views import channels, frontpage
 from channelguide.guide.models import user
 from channelguide import db, cache
 import time
@@ -10,6 +10,7 @@ cache.client.get = lambda x: None
 database = db.connect()
 
 def request(_method='GET', _path="/", **kwargs):
+
     class MockRequest:
         connection = database
         method = _method
@@ -22,8 +23,14 @@ def request(_method='GET', _path="/", **kwargs):
         # middleware would do this, but I'm lazy
         user = user.AnonymousUser()
         total_channels = 0
+
     return MockRequest()
+
 while True:
     channels.popular(request(view='month'))
+    channels.popular(request(view="alltime'))
+    frontpage.get_popular_channels(database, 7)
+    for category in frontpage.get_categories(database):
+        frontpage.get_category_channels(database, category)
     print 'updated'
     time.sleep(60)
