2ff1e0d6b9552955d887c8e4dbc6cb58b199e90e
pswartz <pswartz@be7adf91-e322-0410-8f47-e6edb61c52aa>
2007-10-18 17:45:01 +0000

    better looking first time page
    git-svn-id: https://svn.participatoryculture.org/svn/channelguide2/trunk@989 be7adf91-e322-0410-8f47-e6edb61c52aa

--- channelguide/guide/views/firsttime.py
+++ channelguide/guide/views/firsttime.py
@@ -1,4 +1,15 @@
-from django.http import HttpResponse
+from channelguide import cache, util
 
+@cache.aggresively_cache
+@cache.cache_page_externally_for(3600)
 def index(request):
-    return HttpResponse('First time!')
+    faqs = [
+            ('Question 1?', 'Answer 1'),
+            ('Question 2?', 'Answer 2'),
+            ('Question 3?', 'Answer 3'),
+            ('Question 4?', 'Answer 4'),
+            ('Question 5?', 'Answer 5'),
+            ('Question 6?', 'Answer 6'),
+            ]
+    return util.render_to_response(request, 'firsttime.html',
+            { 'faqs': faqs})
