57173c1f072111817c9db0ff1ce2e49eb978ef6e
pswartz <pswartz@be7adf91-e322-0410-8f47-e6edb61c52aa>
2007-10-24 18:33:11 +0000

    fix error during testing
    git-svn-id: https://svn.participatoryculture.org/svn/channelguide2/trunk@1008 be7adf91-e322-0410-8f47-e6edb61c52aa

--- channelguide/db/updates/34_create_indicies.sql
+++ channelguide/db/updates/34_create_indicies.sql
@@ -1,3 +1,3 @@
 -- create some indicies to make queries faster
-create index by_ip on cg_channel_subscription (ip_address); -- from matt
+create index by_ip on cg_channel_subscription (ip_address); 
 create index channel_id_4 on cg_channel_subscription (channel_id, timestamp, ip_address, ignore_for_recommendations);
