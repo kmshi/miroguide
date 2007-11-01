16aca3517545f33f8a0dba4f9141041f64844cb0
pswartz <pswartz@be7adf91-e322-0410-8f47-e6edb61c52aa>
2007-11-01 03:25:19 +0000

    add a space so as not to confuse the update parser
    git-svn-id: https://svn.participatoryculture.org/svn/channelguide2/trunk@1043 be7adf91-e322-0410-8f47-e6edb61c52aa

--- channelguide/db/updates/37_channel_last_approved.sql
+++ channelguide/db/updates/37_channel_last_approved.sql
@@ -2,6 +2,6 @@ create table cg_channel_last_approved
 (
     timestamp DATETIME,
     PRIMARY KEY (timestamp)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
 -- can't do DEFAULT NOW()
 INSERT INTO cg_channel_last_approved VALUES (NOW());
