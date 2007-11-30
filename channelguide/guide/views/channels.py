7b56cc9ee04cf4048009d5de97dd5b7461b71f11
pswartz <pswartz@be7adf91-e322-0410-8f47-e6edb61c52aa>
2007-11-30 20:49:56 +0000

    fix error
    git-svn-id: https://svn.participatoryculture.org/svn/channelguide2/trunk@1214 be7adf91-e322-0410-8f47-e6edb61c52aa

--- channelguide/guide/views/channels.py
+++ channelguide/guide/views/channels.py
@@ -247,7 +247,8 @@ def show(request, id, featured_form=None):
             'notes.user', 'rating')
     if request.user.is_supermoderator():
         query.join('featured_by', 'featured_queue')
-    c = util.get_object_or_404(request.connection, Channel, id)
+    item_query = Item.query(channel_id=id).join('channel').order_by('date', desc=True).limit(4)
+    c = util.get_object_or_404(request.connection, query, id)
     if c.rating is None:
         c.rating = GeneratedRatings()
         c.rating.channel_id = c.id
