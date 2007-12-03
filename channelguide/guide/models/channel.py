8cad2bed4d609f9619087a98f685377891d37b1d
pswartz <pswartz@be7adf91-e322-0410-8f47-e6edb61c52aa>
2007-12-03 13:57:11 +0000

    don't need to unicodify search data; it's already unicode
    git-svn-id: https://svn.participatoryculture.org/svn/channelguide2/trunk@1216 be7adf91-e322-0410-8f47-e6edb61c52aa

--- channelguide/guide/models/channel.py
+++ channelguide/guide/models/channel.py
@@ -451,7 +451,7 @@ WHERE channel_id=%%s AND %s)""" % ignoresWhere
         for attr in ('tags', 'categories', 'secondary_languages'):
             for obj in getattr(self, attr):
                 values.append(obj.name)
-        values = [util.unicodify(v) for v in values]
+        #values = [util.unicodify(v) for v in values]
         return u' '.join(values)
 
     def get_missing_image_url(self, width, height):
