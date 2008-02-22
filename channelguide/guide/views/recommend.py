6d4e22ea8380c94e669c325da9f556af2f4480ad
pswartz <pswartz@be7adf91-e322-0410-8f47-e6edb61c52aa>
2008-02-22 17:12:25 +0000

    add paging
    git-svn-id: https://svn.participatoryculture.org/svn/channelguide2/trunk@1460 be7adf91-e322-0410-8f47-e6edb61c52aa

--- channelguide/guide/views/recommend.py
+++ channelguide/guide/views/recommend.py
@@ -4,39 +4,55 @@ from channelguide.guide.auth import moderator_required
 from channelguide import util
 import operator
 
+class RecommendationsPager(templateutil.ManualPager):
+
+    def __init__(self, items_per_page, request):
+        self.request = request
+
+        if request.user.is_admin():
+            user_id = int(request.GET.get('user_id', request.user.id))
+        else:
+            user_id = request.user.id
+        ratings = Rating.query(user_id=user_id).execute(request.connection)
+        (recommendations,
+            reasons) = request.user.get_recommendations_from_ratings(
+                request.connection, ratings)
+        toSort = recommendations.items()
+        toSort.sort(key=operator.itemgetter(1), reverse=True)
+        self.recommendations = recommendations
+        self.reasons = reasons
+        self.ids = ids = [cid for (cid, rating) in toSort if rating>=2.25]
+        templateutil.ManualPager.__init__(self, items_per_page, len(self.ids),
+                self._items_callback, request)
+
+    def _items_callback(self, start, length):
+        ids = self.ids[start:start+length]
+        if ids:
+            query = Channel.query(Channel.c.id.in_(ids))
+            query.join('rating')
+            channels = query.execute(self.request.connection)
+            for channel in channels:
+                channel.confidence = int(self.recommendations[channel.id] * 20)
+                if channel.confidence == 100:
+                    channel.confidence = 99 # artificially cap it
+                channelReasons = self.reasons[channel.id][-3:]
+                channelReasons = dict((cid, score) for (score, cid) in channelReasons)
+                query = Channel.query(Channel.c.id.in_(channelReasons.keys()))
+                channel.reasons = query.execute(self.request.connection)
+                for reason in channel.reasons:
+                    reason.score = channelReasons[reason.id]
+                channel.reasons = list(channel.reasons)
+                channel.reasons.sort(key=operator.attrgetter('score'), reverse=True)
+            channels = list(channels)
+            channels.sort(key=operator.attrgetter('confidence'), reverse=True)
+            return channels
+        else:
+            return []
+
 @moderator_required
 def index(request):
-    if request.user.is_admin():
-        user_id = int(request.GET.get('user_id', request.user.id))
-    else:
-        user_id = request.user.id
-    ratings = Rating.query(user_id=user_id).execute(request.connection)
-    recommendations, reasons = request.user.get_recommendations_from_ratings(
-            request.connection, ratings)
-    toSort = recommendations.items()
-    toSort.sort(key=operator.itemgetter(1), reverse=True)
-    ids = [cid for (cid, rating) in toSort[:10]]
-    if ids:
-        query = Channel.query(Channel.c.id.in_(ids))
-        query.join('rating')
-        channels = query.execute(request.connection)
-        for channel in channels:
-            channel.confidence = int(recommendations[channel.id] * 20)
-            if channel.confidence == 100:
-                channel.confidence = 99 # artificially cap it
-            channelReasons = reasons[channel.id][-3:]
-            channelReasons = dict((cid, score) for (score, cid) in channelReasons)
-            query = Channel.query(Channel.c.id.in_(channelReasons.keys()))
-            channel.reasons = query.execute(request.connection)
-            for reason in channel.reasons:
-                reason.score = channelReasons[reason.id]
-            channel.reasons = list(channel.reasons)
-            channel.reasons.sort(key=operator.attrgetter('score'), reverse=True)
-        channels = list(channels)
-        channels.sort(key=operator.attrgetter('confidence'), reverse=True)
-    else:
-        channels = []
-    context = {'channels': channels,
+    pager = RecommendationsPager(10, request)
+    context = {'pager': pager,
             'title': "Channels You'll &hearts;"
         }
     return util.render_to_response(request, 'recommend.html', context)
