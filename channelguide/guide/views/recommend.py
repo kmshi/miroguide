837630112a694f98d0b1478fa9f1895c3f1b7c78
pswartz <pswartz@be7adf91-e322-0410-8f47-e6edb61c52aa>
2008-03-06 20:51:42 +0000

    cache the recommendations
    git-svn-id: https://svn.participatoryculture.org/svn/channelguide2/trunk@1504 be7adf91-e322-0410-8f47-e6edb61c52aa

--- channelguide/guide/views/recommend.py
+++ channelguide/guide/views/recommend.py
@@ -4,6 +4,7 @@
 from channelguide.guide.models import Channel, Rating
 from channelguide.guide import templateutil, recommendations
 from channelguide import util
+from channelguide.cache import client
 import operator
 
 class RecommendationsPager(templateutil.ManualPager):
@@ -15,16 +16,32 @@ class RecommendationsPager(templateutil.ManualPager):
             user_id = int(request.GET.get('user_id', request.user.id))
         else:
             user_id = request.user.id
-        ratings = Rating.query(user_id=user_id).execute(request.connection)
-        (channels,
-            reasons) = recommendations.get_recommendations_from_ratings(
-                request.connection, ratings)
-        toSort = channels.items()
-        toSort.sort(key=operator.itemgetter(1), reverse=True)
-        self.recommendations = channels
-        self.reasons = reasons
-        ids = [cid for (cid, rating) in toSort if rating>=3.25]
-        self.ids = ids[:10 * items_per_page]
+        query = Rating.query(user_id=user_id).order_by(Rating.c.timestamp)
+        ratings = query.execute(request.connection)
+        if ratings:
+            cacheKey = ':'.join(('recommendations_for', str(user_id),
+                str(ratings[-1].timestamp.isoformat())))
+            result = client.get(cacheKey)
+            if result is None:
+                (channels,
+                    reasons) = recommendations.get_recommendations_from_ratings(
+                        request.connection, ratings)
+                toSort = channels.items()
+                toSort.sort(key=operator.itemgetter(1), reverse=True)
+                self.recommendations = channels
+                self.reasons = reasons
+                ids = [cid for (cid, rating) in toSort if rating>=3.25]
+                self.ids = ids[:10 * items_per_page]
+                for id in self.recommendations.keys():
+                    if id not in self.ids:
+                        del self.recommendations[id]
+                        del self.reasons[id]
+                result = self.recommendations, self.reasons, self.ids
+                client.set(cacheKey, result)
+            else:
+                self.recommendations, self.reasons, self.ids = result
+        else:
+            self.ids = []
         templateutil.ManualPager.__init__(self, items_per_page, len(self.ids),
                 self._items_callback, request)
         if self.links.next:
