013b65b2f40b994c4de0f5fd142b5f8851bfcb32
pswartz <pswartz@be7adf91-e322-0410-8f47-e6edb61c52aa>
2008-03-24 15:10:22 +0000

    don't use response.headers
    git-svn-id: https://svn.participatoryculture.org/svn/channelguide2/trunk@1549 be7adf91-e322-0410-8f47-e6edb61c52aa

--- channelguide/cache/decorators.py
+++ channelguide/cache/decorators.py
@@ -11,7 +11,7 @@ def cache_page_externally_for(seconds):
     def decorator(func):
         def inner(*args, **kwargs):
             response = func(*args, **kwargs)
-            response.headers['Cache-Control'] = 'max-age=%d' % seconds
+            response['Cache-Control'] = 'max-age=%d' % seconds
             return response
         return inner
     return decorator
