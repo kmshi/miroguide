d8b854b0e5d0e6379ca56f178f5803da825b8a7a
pswartz <pswartz@be7adf91-e322-0410-8f47-e6edb61c52aa>
2008-03-28 18:42:18 +0000

    put archived channels at the end
    git-svn-id: https://svn.participatoryculture.org/svn/channelguide2/trunk@1657 be7adf91-e322-0410-8f47-e6edb61c52aa

--- channelguide/guide/views/cobranding.py
+++ channelguide/guide/views/cobranding.py
@@ -39,6 +39,7 @@ def cobranding(request, cobrand_name):
     user = User.query(User.c.username==cobrand_name).join('channels').execute(request.connection)[0]
     query = Channel.query().where(Channel.c.owner_id==user.id)
     query.where(Channel.c.state.in_((Channel.APPROVED, Channel.AUDIO)))
+    query.order_by(Channel.c.archived)
     query.order_by(Channel.c.hi_def, desc=True)
     query.order_by(Channel.c.name)
     pager = templateutil.Pager(6, query, request)
