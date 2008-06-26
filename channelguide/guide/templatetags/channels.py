76e1951544bfa9822cec7d71777e928198f01b5e
pswartz <pswartz@be7adf91-e322-0410-8f47-e6edb61c52aa>
2008-06-26 15:20:54 +0000

    factor out channel displays into a template
    git-svn-id: https://svn.participatoryculture.org/svn/channelguide2/branches/streaming@1821 be7adf91-e322-0410-8f47-e6edb61c52aa

--- channelguide/guide/templatetags/channels.py
+++ channelguide/guide/templatetags/channels.py
@@ -2,54 +2,42 @@
 # See LICENSE for details.
 
 from django.conf import settings
-from django.template import Library
-from channelguide.guide.models import Rating, GeneratedRatings
-register = Library()
+from django import template
 
-@register.inclusion_tag('guide/channel-moderate.html', takes_context=True)
-def show_channel_moderate(context, channel, showScript=True):
-    user = context['user']
-    return {'channel': channel, 'user': user,
-            'can_edit': user.can_edit_channel(channel),
-            'show_script': showScript,
-            'STATIC_BASE_URL': settings.STATIC_BASE_URL}
+register = template.Library()
 
-@register.inclusion_tag('guide/edit-bar.html', takes_context=True)
-def show_edit_bar(context, channel, showScript=True):
-    return {'channel': channel, 'user': context['user'],
-            'STATIC_BASE_URL': settings.STATIC_BASE_URL,
-            'show_script': showScript}
+class ChannelNode(template.Node):
+    def __init__(self, channelVariable, features):
+        self.channelVariable = channelVariable
+        self.features = features
 
-@register.inclusion_tag('guide/moderate-actions.html', takes_context=True)
-def show_moderate_actions(context, channel):
-    return {'channel': channel, 'STATIC_BASE_URL': settings.STATIC_BASE_URL,
-            'user': context['user']}
+    def render(self, context):
+        channel = self.channelVariable.resolve(context)
+        t = template.loader.get_template('guide/channel.html')
+        self.nodelist = t.nodelist
+        new_context = template.context.Context(self.features)
+        new_context['channel'] = channel
+        new_context.update(context)
+        return self.nodelist.render(
+            template.context.Context(new_context,
+                                     autoescape=context.autoescape))
 
-@register.inclusion_tag('guide/simple-moderate-actions.html',
-        takes_context=True)
-def show_simple_moderate_actions(context, channel):
-    return {'channel': channel, 'STATIC_BASE_URL': settings.STATIC_BASE_URL,
-            'user': context['user']}
+@register.tag('channel')
+def channel(parser, token):
+    tokens = token.split_contents()
+    print tokens
+    if len(tokens) < 2:
+        raise template.TemplateSyntaxError(
+            'syntax is {% channel <channel> [description] [buttons] \
+[featured] %}')
+    features = {}
+    for token in tokens[2:]:
+        features[token] = True
+    if 'featured' in features:
+        features['buttons'] = features['description'] = True
+    return ChannelNode(template.Variable(tokens[1]), features)
 
-@register.inclusion_tag('guide/channel-small.html', takes_context=True)
-def channel_small(context, channel):
-    return {'request': context['request'], 'channel': channel,
-            'STATIC_BASE_URL': settings.STATIC_BASE_URL }
 
-@register.inclusion_tag('guide/personalized-recommendation.html', takes_context=True)
-def show_personalized_recommendation(context, channel):
-    return {'channel': channel, 'STATIC_BASE_URL': settings.STATIC_BASE_URL,
-            'request': context['request']}
-
-@register.inclusion_tag('guide/channel-mini.html', takes_context=True)
-def channel_mini(context, channel, count):
-    return {'channel': channel, 'count': count, 
-            'STATIC_BASE_URL': settings.STATIC_BASE_URL,
-            'request': context['request']
-            }
-
-    return { 'channel': channel }
-
-@register.inclusion_tag('guide/item.html')
-def item(item):
-    return {'item': item}
+@register.inclusion_tag('guide/button_block.html', takes_context=True)
+def button_block(context, channel):
+    return context
