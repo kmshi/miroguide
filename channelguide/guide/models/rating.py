ca8824a64eb25820f3f73c14c443d90af4d20111
pswartz <pswartz@be7adf91-e322-0410-8f47-e6edb61c52aa>
2008-07-01 20:24:24 +0000

    remove unused imports
    git-svn-id: https://svn.participatoryculture.org/svn/channelguide2/trunk@1868 be7adf91-e322-0410-8f47-e6edb61c52aa

--- channelguide/guide/models/rating.py
+++ channelguide/guide/models/rating.py
@@ -1,10 +1,9 @@
 # Copyright (c) 2008 Participatory Culture Foundation
 # See LICENSE for details.
 
-from channelguide import util
 from channelguide.guide import tables
 from sqlhelper.orm import Record
-from sqlhelper.orm.columns import Subquery
+
 class Rating(Record):
     table = tables.channel_rating
 
