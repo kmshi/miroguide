a84febd0fde795f68446a6e059e798540676ac12
pswartz <pswartz@be7adf91-e322-0410-8f47-e6edb61c52aa>
2008-07-02 18:19:30 +0000

    refactor rating code into stars and meta (avg of...)
    git-svn-id: https://svn.participatoryculture.org/svn/channelguide2/branches/streaming@1883 be7adf91-e322-0410-8f47-e6edb61c52aa

--- channelguide/guide/templatetags/rating.py
+++ channelguide/guide/templatetags/rating.py
@@ -13,16 +13,19 @@ def rating_header(parser, tokens):
 <link rel="StyleSheet" type="text/css" href="%scss/rating.css" />""" % (
         settings.STATIC_BASE_URL, settings.STATIC_BASE_URL))
 
-@register.inclusion_tag('guide/rating.html', takes_context=True)
-def rating(context, channel):
-    request = context['request']
-    user_rating = False
+def _setup_channel(channel, connection):
     if not hasattr(channel, 'rating'):
-        channel.join('rating').execute(request.connection)
+        channel.join('rating').execute(connection)
     if not channel.rating:
         channel.rating = GeneratedRatings()
         channel.rating.channel_id = channel.id
-        channel.rating.save(request.connection)
+        channel.rating.save(connection)
+
+@register.inclusion_tag('guide/rating_stars.html', takes_context=True)
+def rating(context, channel):
+    request = context['request']
+    _setup_channel(channel, request.connection)
+    user_rating = False
     try:
         rating = Rating.query(Rating.c.user_id==request.user.id,
                               Rating.c.channel_id==channel.id).get(request.connection)
@@ -38,3 +41,9 @@ def rating(context, channel):
         'class': (user_rating and 'userrating' or 'averagerating'),
         'referer': request.path,
         }
+
+@register.inclusion_tag('guide/rating_meta.html', takes_context=True)
+def rating_meta(context, channel):
+    request = context['request']
+    _setup_channel(channel, request.connection)
+    return {'channel': channel}
