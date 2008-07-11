651320b4f4e56e88e0076a589366d2f63643f60e
pswartz <pswartz@be7adf91-e322-0410-8f47-e6edb61c52aa>
2008-07-11 15:03:50 +0000

    be more careful showing/hiding load indicator
    git-svn-id: https://svn.participatoryculture.org/svn/channelguide2/trunk@1945 be7adf91-e322-0410-8f47-e6edb61c52aa

--- static/js/miroguide.js
+++ static/js/miroguide.js
@@ -37,14 +37,14 @@ function getNextElement(elt) {
 }
 
 function showLoadIndicator(always) {
-    if (always || navigator.userAgent.indexOf('Miro') != -1) {
-	indicator = $("#load-indicator");
+    indicator = $("#load-indicator");
+    if ((!indicator.queue().length) && always || navigator.userAgent.indexOf('Miro') != -1) {
 	indicator.animate({bottom: 0}, 'fast');
     }
 }
 
 function hideLoadIndicator() {
-    indicator = $("#load-indicator").css('bottom', '-30px');
+    indicator = $("#load-indicator").stop().css('bottom', '-30px');
 }
 
 function makeXMLHttpRequest() {
