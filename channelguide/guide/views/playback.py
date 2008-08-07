73c9730a3373e6d7549c153d5ce3b87ddf885159
Paul Swartz <pswartz@pculture.org>
2008-08-07 14:11:57 -0400

    flash embed

--- channelguide/guide/views/playback.py
+++ channelguide/guide/views/playback.py
@@ -1,7 +1,7 @@
 from channelguide import util
 from channelguide.guide.models import Item
 DEFAULT_WIDTH = 480
-DEFAULT_HEIGHT= 375
+DEFAULT_HEIGHT= 360
 
 def quicktime_embed(item):
     return """<object id="video" classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" width="%i" height="%i" bgcolor="ffffff" codebase="http://www.apple.com/qtactivex/qtplugin.cab">
@@ -13,8 +13,20 @@ def quicktime_embed(item):
 <embed src="%s" bgcolor="ffffff" name="movieobject" scale="aspect" width="%i" height="%i" id="video" autoplay="true" controller="true" pluginspage="http://www.apple.com/quicktime/download/">
 </embed>
 </object>
-""" % (DEFAULT_WIDTH, DEFAULT_HEIGHT, item.url, item.url, DEFAULT_WIDTH,
-       DEFAULT_HEIGHT)
+""" % (DEFAULT_WIDTH, DEFAULT_HEIGHT + 15, item.url, item.url, DEFAULT_WIDTH,
+       DEFAULT_HEIGHT + 15)
+
+def flash_embed(item):
+    return """<object width="%i" height="%i" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000">
+<param value="%s" name="movie"/>
+<param value="true" name="allowfullscreen"/>
+<param value="high" name="quality"/>
+<param value="autoplay=1" name="FlashVars"/>
+<param value="transparent" name="wmode"/>
+<param value="video" name="id"/>
+<embed id="video" width="%i" height="%i" allowfullscreen="true" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" wmode="transparent" quality="high" enablehref="false" flashvars="autoplay=1" src="%s\"/>
+</object>""" % (DEFAULT_WIDTH, DEFAULT_HEIGHT + 36, item.url,
+                DEFAULT_WIDTH, DEFAULT_HEIGHT + 36, item.url)
 
 def default_embed(item):
     return """<embed width="%i" height="%i" src="%s"></embed>""" % (
@@ -32,6 +44,9 @@ EMBED_MAPPING = {
     'audio/mp4': quicktime_embed,
     'video/x-mp4': quicktime_embed,
     'audio/mp3': quicktime_embed,
+    'application/x-shockwave-flash': flash_embed,
+    'video/x-flv': flash_embed,
+    'video/flv': flash_embed,
 }
 
 def embed_code(item):
