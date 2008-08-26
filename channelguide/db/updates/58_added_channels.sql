f76acec5a723632c38f428fa47e6229c5407fd86
pswartz <pswartz@46f2bd34-0d25-4fd3-afc4-d03ba3bd0a21>
2008-08-26 16:37:17 +0000

    remove changes that belong in a different branch
    git-svn-id: https://svn.participatoryculture.org/svn/channelguide2/branches/streaming@2213 46f2bd34-0d25-4fd3-afc4-d03ba3bd0a21

--- channelguide/db/updates/58_added_channels.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE cg_channel_added (
-       channel_id INT(11) NOT NULL,
-       user_id INT(11) NOT NULL,
-       timestamp DATETIME,
-       PRIMARY KEY (channel_id, user_id),
-       CONSTRAINT fk_channel_added_user FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE,
-       CONSTRAINT fk_channel_added_channel FOREIGN KEY (channel_id) REFERENCES cg_channel (id) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
