9abf34bf5934a57dae211180ca6a455acbf2b799
Christopher Allan Webber <cwebber@dustycloud.org>
2008-12-18 13:14:08 -0600

    fixing the carousel so that it isn't just stationary
    (cherry-picking retemplating stuff)

---
+++ static/js/feature_rotate.js
@@ -0,0 +1,127 @@
+/* feature_rotate.js
+ * 
+ * Does the feature rotate animation.
+ */
+
+var FEATURED_CHANNEL_WIDTH = 385;
+var FEATURE_ROTATE_TIMEOUT = 15; // rotate timeout in seconds
+var featureList = null;
+var featureTimeout = null;
+var manualMode = true;
+var inRotate = false;
+
+function scheduleFeatureRotate() {
+   if(manualMode) return;
+   // loadHiddenScreenshots();
+   if(!featureList) featureList = findFeatures();
+   featureTimeout = setTimeout(rotateFeatures, FEATURE_ROTATE_TIMEOUT * 1000);
+}
+
+function cancelFeatureRotate() {
+   if(featureTimeout) {
+       clearTimeout(featureTimeout);
+       featureTimeout = null;
+   }
+   manualMode = true;
+}
+
+// function loadHiddenScreenshots() {
+//    var i = 1;
+//    while(true) {
+//      var screenshot = document.getElementById('feature-screenshot-' + i);
+//      i += 1;
+//      if(!screenshot) break;
+//      var fakeScreenshot = screenshot.childNodes[0];
+//      if(fakeScreenshot.nodeType != 3) continue;
+//      var re = /src:\s*"([^"]*)".*alt:\s*"([^"]*)"/
+//      var matches = re.exec(fakeScreenshot.nodeValue);
+//      if(!matches) continue;
+//      var realScreenshot = document.createElement('img'); 
+//      realScreenshot.setAttribute('src', matches[1]);
+//      realScreenshot.setAttribute('alt', matches[2]);
+//      screenshot.replaceChild(realScreenshot, fakeScreenshot);
+//   }
+// }
+
+function rotateFeaturesLeft() {
+    doRotate(Math.max(0, FSCurrentFeature - 3));
+    cancelFeatureRotate();
+}
+
+function rotateFeaturesRight() {
+    doRotate(Math.min(featureList.length-2, FSCurrentFeature + 3));
+    cancelFeatureRotate();
+}
+
+function rotateFeatures() {
+   featureTimeout = null;
+   doRotate(pickNextFeature());
+}
+
+function doRotate(nextFeature) {
+   if(inRotate) return;
+   inRotate = true;
+   FSStartX = FSCurrentFeature * FEATURED_CHANNEL_WIDTH;
+   console.log(FSStartX);
+   FSCurrentFeature = nextFeature;
+   console.log(FSCurrentFeature);
+   FSEndX = FSCurrentFeature * FEATURED_CHANNEL_WIDTH;
+   console.log(FSEndX);
+   FSTime = 0.0;
+   console.log(FSTime);
+   animateFeatureScroll();
+}
+
+
+function pickNextFeature() {
+   var nextFeature = FSCurrentFeature + 2*FSScrollDirection;
+   if(nextFeature < 0) {
+        FSScrollDirection *= -1;
+        return 0
+   }
+   if(nextFeature >= featureList.length-2) {
+        FSScrollDirection *= -1;
+        return featureList.length-2;
+   }
+   return nextFeature;
+}
+
+function findFeatures() {
+   return $('#featured-list').children();
+}
+
+
+// Variables used in the feature scroll animation.  They are prefixed with FS to
+// avoid namespace conflicts.
+var FSStartX = 0;
+var FSEndX = 0;
+var FSTime = 0.0; // Time variable. ranges from 0 to 1.
+var FSTimeStep = 0.1;
+var FSTimeout = 50;
+var FSCurrentFeature = 0;
+var FSScrollDirection = 1;
+
+function animateFeatureScroll() {
+   if(FSTime < 1.0) {
+       moveFeatureList(FSStartX + (FSEndX - FSStartX) * FSCalcPosition());
+       FSTime += FSTimeStep;
+       setTimeout(animateFeatureScroll, FSTimeout);
+   } else {
+       moveFeatureList(FSEndX);
+       inRotate = false;
+       scheduleFeatureRotate();
+   }
+}
+
+function moveFeatureList(leftX) {
+    console.log('movin on up')
+    var featuredList = document.getElementById('featured-list');
+    featuredList.style.left = '-' + leftX + 'px';
+}
+
+// Translates the feature scroll time into a position.  Ranges from 0 to 1,
+// like FSTime, but gets transformed using a cosine for nice, non-linear
+// movement.
+function FSCalcPosition() {
+  return (1 - Math.cos(FSTime * Math.PI)) / 2;
+}
