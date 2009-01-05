942ad58c01b77a63320cbda84d6cc9016a63db1b
Paul Swartz <pswartz@pculture.org>
2009-01-05 16:47:40 -0500

    Merge listview branch into redesign

--- channelguide/guide/api.py
+++ channelguide/guide/api.py
@@ -25,15 +25,9 @@ def get_channel_by_url(connection, url):
                                        'language').get(
         connection)
 
-def get_channels(connection, filter, value, sort=None, limit=None, offset=None,
-                 loads=None):
+def get_channels_query(connection, filter, value, sort=None, loads=None):
     query = Channel.query_approved()
     join = None
-    if loads:
-        joins = [name for name in loads if not getattr(Channel.c, name, False)] 
-        loads = [name for name in loads if getattr(Channel.c, name, False)]
-    else:
-        joins = loads = ()
     if filter == 'category':
         try:
             category_id = Category.query(name=value).get(connection).id
@@ -74,8 +68,10 @@ def get_channels(connection, filter, value, sort=None, limit=None, offset=None,
     elif filter == 'name':
         if value:
             query.where(Channel.c.name.like(value + '%'))
+    elif filter == 'search':
+        query = search_mod.search_channels(value.split())
     else:
-        return []
+        raise ValueError('unknown filter: %r' % (filter,))
     if sort is not None and sort[0] == '-':
         desc = True
         sort = sort[1:]
@@ -100,19 +96,68 @@ def get_channels(connection, filter, value, sort=None, limit=None, offset=None,
         return query.count(connection)
     else:
         raise ValueError('unknown sort type: %r' % sort)
+    return query
+
+def _split_loads(loads):
+    if loads:
+        joins = [name for name in loads if not getattr(Channel.c, name, False)]
+        loads = [name for name in loads if getattr(Channel.c, name, False)]
+    else:
+        joins = loads = ()
+    return joins, loads
+
+def _add_limit_and_offset(query, limit, offset):
     if limit is None:
         limit = 20
     if limit > 100:
         limit = 100
     if offset is None or offset < 0:
         offset = 0
-    query.limit(limit).offset(offset).load(*loads)
+    return query.limit(limit).offset(offset)
+
+def get_feeds(connection, filter, value, sort=None, limit=None, offset=None,
+              loads=None):
+    query = get_channels_query(connection, filter, value, sort=sort)
+    if isinstance(query, long):
+        return query
+    _add_limit_and_offset(query, limit, offset)
+    query.where(Channel.c.url.is_not(None))
+    joins, loads = _split_loads(loads)
+    query.load(*loads)
     results = query.execute(connection)
     if results:
         results.join(*joins).execute(connection)
-    if join:
-        for result in results:
-            delattr(result, join)
+    return results
+
+def get_shows(connection, filter, value, sort=None, limit=None, offset=None,
+              loads=None):
+    query = get_channels_query(connection, filter, value, sort=sort)
+    if isinstance(query, long):
+        return query
+    _add_limit_and_offset(query, limit, offset)
+    query.where(Channel.c.url.is_(None))
+    joins, loads = _split_loads(loads)
+    query.load(*loads)
+    results = query.execute(connection)
+    if results:
+        results.join(*joins).execute(connection)
+    return results
+
+def get_channels(connection, filter, value, sort=None, limit=None, offset=None,
+                 loads=None):
+    """
+    The old API method which returns a list of channels.  With the redesign and
+    the inclusion of sites, you should use either get_feeds or get_sites.
+    """
+    query = get_channels_query(connection, filter, value, sort=sort)
+    if isinstance(query, long):
+        return query
+    _add_limit_and_offset(query, limit, offset)
+    joins, loads = _split_loads(loads)
+    query.load(*loads)
+    results = query.execute(connection)
+    if results:
+        results.join(*joins).execute(connection)
     return results
 
 def search(connection, terms):
@@ -138,7 +183,7 @@ def get_ratings(connection, user, rating=None):
                 Rating.query(user_id=user.id, rating=rating).join(
                 'channel').execute(connection)]
 
-def get_recommendations(connection, user, start=0, length=10):
+def get_recommendations(connection, user, start=0, length=10, filter=None):
     rating_query = Rating.query(user_id=user.id).order_by(Rating.c.timestamp)
     ratings = rating_query.execute(connection)
     added_query = AddedChannel.query(user_id=user.id).order_by(
@@ -174,11 +219,21 @@ def get_recommendations(connection, user, start=0, length=10):
             client.set(cacheKey, result)
         else:
             estimatedRatings, reasons, ids = result
+        query = Channel.query(Channel.c.id.in_(ids))
+        if filter is not None:
+            if filter == 'feed':
+                query.where(Channel.c.url.is_not(None))
+            elif filter == 'show':
+                query.where(Channel.c.url.is_(None))
+            else:
+                raise ValueError('unknown recommendations filter: %r' % filter)
         if start is None:
-            return len(ids)
+            if not ids:
+                return 0
+            else:
+                return query.count(connection)
         if not ids:
             return []
-        query = Channel.query(Channel.c.id.in_(ids))
         query.join('rating')
         channels = list(query.execute(connection))
         for channel in channels:
