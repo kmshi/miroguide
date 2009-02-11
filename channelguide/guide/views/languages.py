e3a42969e7c8b6f4b25781f9d8d4a41b9c2b1723
Paul Swartz <pswartz@pculture.org>
2009-02-11 17:24:22 -0500

    cache the Language index query for an hour

--- channelguide/guide/views/languages.py
+++ channelguide/guide/views/languages.py
@@ -11,6 +11,8 @@ from channelguide.guide.models import Language
 @cache.aggresively_cache
 def index(request):
     query = Language.query().load('channel_count').order_by('name')
+    query.cacheable = cache.client
+    query.cacheable_time = 3600
     return util.render_to_response(request, 'group-list.html', {
         'group_name': _('Shows by Language'),
         'groups': query.execute(request.connection),
