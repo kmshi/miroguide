b04605ad12c497badcdba0a9eac08d4cd6247f48
Paul Swartz <pswartz@pculture.org>
2009-02-11 17:55:31 -0500

    set default language for the user

--- channelguide/guide/models/user.py
+++ channelguide/guide/models/user.py
@@ -88,6 +88,7 @@ class User(UserBase, Record):
     def __init__(self, username, password, email):
         self.username = username
         self.email = email
+        self.language = ''
         self.set_password(password)
 
     def get_url(self):
