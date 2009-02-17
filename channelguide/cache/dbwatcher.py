c2ad60f5921484efc152aa03925d26576ab5d59c
Paul Swartz <pswartz@pculture.org>
2009-02-17 10:43:47 -0500

    don't clear cache when adding new users

--- channelguide/cache/dbwatcher.py
+++ channelguide/cache/dbwatcher.py
@@ -9,9 +9,8 @@ from client import set
 def handle_change(statement):
     # reset the table key, so cached pages will not find the old caches
     if statement.table_name not in ('cg_session', 'cg_channel_subscription',
-                                    'cg_channel_subscription_holding'):
+                                    'cg_channel_subscription_holding', 'user'):
         set('namespace', time.time())
-#    set('Table:' + statement.table_name, time.time())
 
 signals.sql_insert.connect(handle_change)
 signals.sql_update.connect(handle_change)
