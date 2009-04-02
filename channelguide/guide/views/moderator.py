9d9765e7c99f524c438330419c82e1c37bedcb21
Paul Swartz <pswartz@pculture.org>
2009-04-02 12:19:12 -0400

    calculate the statistics based on the actual days/months

--- channelguide/guide/views/moderator.py
+++ channelguide/guide/views/moderator.py
@@ -124,19 +124,21 @@ def stats(request):
     month_keys = [('last_31_days', months[0])]
 
     for i in range(1, 100):
-        key = 'stats:day:%i:%i:%i' % (date.today() - timedelta(days=i)).timetuple()[:3]
+        tt = (date.today() - timedelta(days=i)).timetuple()[:3]
+        key = 'stats:day:%i:%i:%i' % tt
         val = cache.client.get(key)
         if val is None:
-            val = request.connection.execute('SELECT COUNT(*) FROM cg_channel_subscription WHERE timestamp > DATE_SUB(NOW(), INTERVAL %s DAY) AND timestamp < DATE_SUB(NOW(), INTERVAL %s DAY)', (i+1, i))[0][0]
+            val = request.connection.execute('SELECT COUNT(*) FROM cg_channel_subscription WHERE YEAR(timestamp) = %s AND MONTH(timestamp) = %s AND DAY(timestamp) = %s', tt)[0][0]
             cache.client.set(key, val)
         today_keys.append((key, val))
         todays.append(val)
 
     for i in range(1, 12):
-        key = 'stats:month:%i:%i' % (date.today() - timedelta(days=30*i)).timetuple()[:2]
+        tt = (date.today() - timedelta(days=30*i)).timetuple()[:2]
+        key = 'stats:month:%i:%i' % tt
         val = cache.client.get(key)
         if val is None:
-            val = request.connection.execute('SELECT COUNT(*) FROM cg_channel_subscription WHERE timestamp > DATE_SUB(NOW(), INTERVAL %s MONTH) AND timestamp < DATE_SUB(NOW(), INTERVAL %s MONTH)', (i+1, i))[0][0]
+            val = request.connection.execute('SELECT COUNT(*) FROM cg_channel_subscription WHERE YEAR(timestamp) = %s AND MONTH(timestamp) = %s', tt)[0][0]
             cache.client.set(key, val)
         month_keys.append((key, val))
         months.append(val)
