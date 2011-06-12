1baaf2d02e6630e9dba906dd3d655ad0ae5c9fbe
kmshi <kmshi_array@yahoo.com>
2011-06-12 08:07:51 +0000

    Fix ip2cc lib path and typechange issue

--- channelguide/manage.py
+++ channelguide/manage.py
@@ -2,7 +2,7 @@
 from django.core.management import execute_manager
 import os, sys
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
+sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__file__)), 'lib'))
 
 try:
     import settings # Assumed to be in the same directory.
